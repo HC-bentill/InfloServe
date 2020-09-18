534772721076604395dbf86be59d79958bbda518
HCB <henrybentil88@gmail.com>
2020-09-18 18:18:50 +0000

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ include('includes/nav.php');
           <h3 id="maintext" class="w3-animate-zoom">
              Glamour • <strong style="color:#abce4a">Style</strong> • Elegance
           </h3>
-          <a href="about-us.php" >
+          <a href="products.php" >
             <button id="know-more"  type="button" class="btn w3-animate-right">
              KNOW MORE
           </button>
@@ -68,7 +68,7 @@ include('includes/nav.php');
         <!-- 1st col -->
         <div class="col-md-4">
           <h1 class="w3-animate-left">
-          Upcoming Events & Destination
+           Events Planning & Rentals
           </h1>
           <div id="underline" ></div> <br>
           <p>Looking for a wedding planner?</p> <br>
@@ -93,7 +93,7 @@ include('includes/nav.php');
           <div class="img-container" >
             <img src="img/img2.jpg" alt="Avatar" class="img-fluid">
             <div class="overlay">
-             <div class="text">Location & Planning</div>
+             <div class="text">Setting & Planning</div>
             </div>          
         </div>
         <div class="img-container mt-4" >
@@ -130,78 +130,97 @@ include('includes/nav.php');
   </section>
   <!-- end of events section -->
 
-  <div class="row text-center">
-    <div class="owl-carousel owl-theme">
+<hr>    
+
+      <!-- start of about us section -->
+    <section class="about-info">
+     <div class="container">
+       <h1>Why Choose Us ? </h1>
+        <div class="row about-text about-img pt-2 g-3">
+          <div class="col-md-6 col-sm-12">
+            <br><br> <strong>Our Vision  :</strong>  Before and after your event; wedding, birthday, conference, family gathering among others.If you want to plan a beautiful event that will make headlines, then prioritise the experts on this list. 
+                  beneficiaries lack the 
+            <br><br> <strong>Our Values  :</strong> not simply a concept. It’s how we develop and maintain customer loyalty.
+            Innovation – it is the driving force behind 
+            <br><br> <strong>Our Passion  :</strong> to access a wider arts community, showcase their skills,
+                        advertise their services, and gain private 
+            
+             </p>
+        </div>
+        <div class="col-md-6">
+            <img class="img-fluid" src="img/img40.jpg" alt=""/>
+        </div> 
+          <div class="mt-3">
+            <p>Social Media Platforms</p>
+          <a href="https://www.facebook.com/Infloservevents/" class="fa fa-facebook"></a><a href="#" class="fa fa-twitter"></a>
+          <a href="https://www.instagram.com/Infloservevents/" class="fa fa-instagram"></a><a href="#" class="fa fa-whatsapp"></a>
+       </div>
+      
+    </div>
+    </section>
+
+
+  <div class="carousel">
+    <div class="row text-center">
+      <div class="container">
+      <div class="col-md-12">
+      <div class="owl-carousel owl-theme">
       <div class="item">
-        <img src="img/img1.jpg" class="img-fluid" alt="">
+        <img src="img/img44.webp" class="img-fluid" alt="">
+      
       </div>
      <div class="item">
-        <img src="img/img2.jpg" class="img-fluid" alt="">
+        <img src="img/img45.jpg" class="img-fluid" alt="">
+     
       </div>
       <div class="item">
-        <img src="img/img3.jpg" class="img-fluid" alt="">
+        <img src="img/img46.jpg" class="img-fluid" alt="">
+              </div>
+      <div class="item">
+        <img src="img/img47.jpg" class="img-fluid" alt="">
+      
       </div>
       <div class="item">
-        <img src="img/img18.jpg" class="img-fluid" alt="">
+        <img src="img/img48.jpg" class="img-fluid" alt="">
+      
       </div>
       <div class="item">
-        <img src="img/img5.jpg" class="img-fluid" alt="">
+        <img src="img/img49.jpg" class="img-fluid" alt="">
       </div>
       <div class="item">
-        <img src="img/img6.jpg" class="img-fluid" alt="">
+        <img src="img/img50.jpg" class="img-fluid" alt="">
+       
+      </div>
+      <div class="item">
+        <img src="img/img51.jpg" class="img-fluid" alt=""> 
       </div>
       
     </div>
+
+      </div>
+      </div>
+    
+    </div>
   </div>
 
-  <!-- owl carousel -->
-  <section class="owl-carousel">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          
-        </div>
-      </div>
-    </div>
-  </section>
-  <!-- end of carousel -->
+  <hr>
 
 
-  <!-- start of Hero -->
-  <section class="home-parallax img-fluid">
-      <div class="container">
-        <div class="row">
-           <div class="col-md-12">
-             <!-- embedded video -->
-             <div class="text-center">
-             <h1 class="font" style="font-size:65px">Video Content of Our Works</h1>
-             <div id="underline"></div> <br>
-             <div class="embed-responsive embed-responsive-16by9">
-               <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/-J5U4V1ZPlc" allowfullscreen></iframe>
-              
-             </div>
 
-             </div>
-
-           </div>
-          </div>
-        </div>
-      </div>
-    </section>
 
     <section class="second-parallax img-fluid">
       <div class="container">
         <div class="row">
           <div class="col-md-12" >
             <div class="text-center"> 
-              <h1 class="font" style="font-size:65px">Planning Your Big day</h1>
+              <h1 class="font animate__bounce" style="font-size:65px">Planning Your Big day</h1>
               <div id="underline" class="w3-animate-zoom"></div>
-              <p>“Identifying a list of tangible and intangible goals for your event can give you a better sense of your 
+              <p>Identifying a list of tangible and intangible goals for your event can give you a better sense of your 
                 event management needs. Your tangible goals may be to attract 500 attendees, sell 100 products at the event, or raise
                  $1 million for charity. Your intangible goals will help drive these tangible goals and may include raising awareness
                  about a product or cause, or recruiting volunteers.” </p>
             </div>
-            <a href="rentals.php" >
+            <a href="products.php" >
             <button id="know-more"  type="button" class="btn w3-animate-right">
              KNOW MORE
           </button>
@@ -215,46 +234,133 @@ include('includes/nav.php');
     
     <!-- end of Hero -->
 
+      <!-- start of Hero -->
+  <section class="home-parallax img-fluid">
+      <div class="container">
+        <div class="row">
+           <div class="col-md-12">
+             <!-- embedded video -->
+             <div class="text-center">
+
+             <div class="embed-responsive embed-responsive-21by9">
+               <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/-J5U4V1ZPlc" allowfullscreen></iframe>
+              
+             </div>
+
+             </div>
+
+           </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
     
 
     <!-- start of testimonials -->
     <section class="testimonials">
       <div class="container">
         <div class="row">
-          <h3>Client Reviews</h3>
+          <h3>Testimonials</h3>
         </div>
       <div class="row p-5 text-center">
           <div class="col-lg-4">
-            <img class="rounded-circle" src="img/avatar1.jfif" alt="Generic placeholder image" width="120" height="120">
-            <h2>Kwadwo Sapong</h2>
-            <h5>Businessman</h5>
-            <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+            <img class="rounded-circle" src="img/img52.jpg" alt="Generic placeholder image" width="120" height="120">
+            <h2>Ivy Sedzifa</h2>
+            <h5>Businesswoman</h5>
+            <p>I was very much impressed with my birthday event beside the touch of class and colourful presentation smmodo cursus magna.</p>
            
           </div><!-- /.col-lg-4 -->
           <div class="col-lg-4">
-            <img class="rounded-circle" src="img/avatar2.jfif" alt="Generic placeholder image" width="120" height="120">
+            <img class="rounded-circle" src="img/img53.jpg" alt="Generic placeholder image" width="120" height="120">
             <h2>Micheal Antwi</h2>
             <h5>Businessman</h5>
-            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+            <p>They are simply the best in executing work and delivering on time.</p>
            
           </div><!-- /.col-lg-4 -->
           <div class="col-lg-4">
-            <img class="rounded-circle" src="img/avatar1.jfif" alt="Generic placeholder image" width="120" height="120">
-            <h2>Daniel Amponsah </h2>
-            <h5>Businessman</h5>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+            <img class="rounded-circle" src="img/img54.jpg" alt="Generic placeholder image" width="120" height="120">
+            <h2>Daniella Amponsah </h2>
+            <h5>Pharmacist</h5>
+            <p>Their services are excellent and they have exquisite taste when it comes to organizing events. </p>
            
           </div><!-- /.col-lg-4 -->
         </div>
       </div>
     </section>
     <!--end of testimonials  -->
+    <!-- start of e-planning -->
+<section class="event-planning">
+  <div class="container">
+    <div class="row event-planning-content text-center">
+      <div class="col-md-12 white-opacity">
+        <button
+          class="btn accordion-btn"
+          type="button"
+          data-toggle="collapse"
+          data-target="#multiCollapseExample2"
+          aria-expanded="false"
+          aria-controls="multiCollapseExample2"
+        >
+          Click to View Our Services
+        </button>
+        <div class="row">
+          <div class="col">
+            <div class="collapse multi-collapse" id="multiCollapseExample2">
+              <div class="card card-body">
+                <ul class="w3-ul w3-card-4">
+                  <li class="w3-bar">
+                    <div class="w3-bar-item">
+                      <span class="w3-large">Party rentals</span><br />
+                    </div>
+                  </li>
+                  <li class="w3-bar">
+                    <div class="w3-bar-item">
+                      <span class="w3-large">Event planning </span><br />
+                    </div>
+                  </li>
+
+                  <li class="w3-bar">
+                    <div class="w3-bar-item">
+                      <span class="w3-large">Event setups</span><br />
+                    </div>
+                  </li>
+                  <li class="w3-bar">
+                    <div class="w3-bar-item">
+                      <span class="w3-large">Garden weddings</span><br />
+                    </div>
+                  </li>
+                  <li class="w3-bar">
+                    <div class="w3-bar-item">
+                      <span class="w3-large">Church weddings</span><br />
+                    </div>
+                  </li>
+                  <li class="w3-bar">
+                    <div class="w3-bar-item">
+                      <span class="w3-large">Corporate events etc.</span><br />
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- end of e-planning -->
 
     
   <hr>
 
   <section class="companies">
     <div class="container">
+    <div class="row">
+      <div class="col-md-12 text-center">
+          <h3>Clients We Have Proudly Served</h3>
+    </div>
+    </div>
       <div class="row text-center">
         <div class="col-md-2"><img src="img/company1.jpg" alt="" class="img-fluid" /></div>
         <div class="col-md-2"><img src="img/company2.jpeg" alt="" class="img-fluid" /></div>
